e778cd6232a1549073b65475301a9d5e1d71012f
PhilippeIntegTest <Philippe.chesnel_Gith1@gadzarts.org>
2024-04-08 10:40:42 +0200

    [Branch_Emn_Test] Add Button Emn

--- Windows_App_Git_Test/Form1.Designer.vb
+++ Windows_App_Git_Test/Form1.Designer.vb
@@ -24,6 +24,7 @@ Partial Class Git_Test_V1
 	Private Sub InitializeComponent()
 		Me.components = New System.ComponentModel.Container()
 		Me.U_Db_Database_Access1 = New Common_All_2013_a.U_Db_Database_Access()
+		Me.Button1 = New System.Windows.Forms.Button()
 		Me.SuspendLayout()
 		'
 		'U_Db_Database_Access1
@@ -32,23 +33,33 @@ Partial Class Git_Test_V1
 		Me.U_Db_Database_Access1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
 		Me.U_Db_Database_Access1.ForeColor = System.Drawing.Color.Black
 		Me.U_Db_Database_Access1.Is_DB_Create_And_Structured = False
-		Me.U_Db_Database_Access1.Location = New System.Drawing.Point(164, 153)
+		Me.U_Db_Database_Access1.Location = New System.Drawing.Point(118, 178)
 		Me.U_Db_Database_Access1.Name = "U_Db_Database_Access1"
 		Me.U_Db_Database_Access1.Nom_Complet_Fichier_Database = "C:\Test_Auto.mdb"
 		Me.U_Db_Database_Access1.Nom_Complet_Fichier_Database_Reference = "NC"
 		Me.U_Db_Database_Access1.Nom_Path_Fichier_Database = "NC"
 		Me.U_Db_Database_Access1.Nom_Simple_Fichier_Database = "NC"
-		Me.U_Db_Database_Access1.Size = New System.Drawing.Size(1042, 337)
+		Me.U_Db_Database_Access1.Size = New System.Drawing.Size(75, 57)
 		Me.U_Db_Database_Access1.TabIndex = 0
 		Me.U_Db_Database_Access1.Type_Path_Fichier_Database = Common_All_2013_a.U_Db_Database_Access.E_Type_Path_Fichier_Database.Path_Exe_Database
 		Me.U_Db_Database_Access1.Use_Referenced_DB = False
+		'
+		'Button1
+		'
+		Me.Button1.Location = New System.Drawing.Point(118, 12)
+		Me.Button1.Name = "Button1"
+		Me.Button1.Size = New System.Drawing.Size(200, 23)
+		Me.Button1.TabIndex = 1
+		Me.Button1.Text = "Branch Emn Test"
+		Me.Button1.UseVisualStyleBackColor = True
 		'
 		'Git_Test_V1
 		'
 		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
 		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
 		Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
-		Me.ClientSize = New System.Drawing.Size(1213, 497)
+		Me.ClientSize = New System.Drawing.Size(610, 295)
+		Me.Controls.Add(Me.Button1)
 		Me.Controls.Add(Me.U_Db_Database_Access1)
 		Me.Name = "Git_Test_V1"
 		Me.Text = "Git_Test_V1"
@@ -57,4 +68,5 @@ Partial Class Git_Test_V1
 	End Sub
 
 	Friend WithEvents U_Db_Database_Access1 As Common_All_2013_a.U_Db_Database_Access
+	Friend WithEvents Button1 As Button
 End Class
